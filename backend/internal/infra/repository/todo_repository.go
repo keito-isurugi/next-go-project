210b6772d6b0d90df4df52d3125b50880f8b74fb
keito-isurugi <ja224kg@gmail.com>
2024-07-22 03:39:15 +0900

    infra修正

--- backend/internal/infra/repository/todo_repository.go
+++ backend/internal/infra/repository/todo_repository.go
@@ -23,5 +23,59 @@ func (tr *todoRepository) ListTodos(ctx context.Context) (entity.ListTodos, erro
 	if err := tr.dbClient.Conn(ctx).Where("done_flag", false).Order("id").Find(&lt).Error; err != nil {
 		return entity.ListTodos{}, err
 	}
-	return entity.ListTodos{}, nil
+
+	return lt, nil
+}
+
+func (tr *todoRepository) GetTodo(ctx context.Context, id int) (entity.Todo, error) {
+	var t entity.Todo
+	if err := tr.dbClient.Conn(ctx).
+		Where("id", id).
+		First(&t).Error; err != nil {
+		return entity.Todo{}, err
+	}
+
+	return t, nil
+}
+
+func (tr *todoRepository) RegisterTodo(ctx context.Context, todo *entity.Todo) (int, error) {
+	if err := tr.dbClient.Conn(ctx).
+		Create(todo).
+		Error; err != nil {
+		return 0, err
+	}
+
+	return todo.ID, nil
+}
+
+func (tr *todoRepository) UpdateTodo(ctx context.Context, todo *entity.Todo) error {
+	var t entity.Todo
+	if err := tr.dbClient.Conn(ctx).Where("id", todo.ID).First(&t).Error; err != nil {
+		return err
+	}
+
+	todo.CreatedAt = t.CreatedAt
+
+	if err := tr.dbClient.Conn(ctx).
+		Updates(todo).
+		Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tr *todoRepository) DeleteTodo(ctx context.Context, id int) error {
+	var t entity.Todo
+	if err := tr.dbClient.Conn(ctx).Where("id", id).First(&t).Error; err != nil {
+		return err
+	}
+
+	if err := tr.dbClient.Conn(ctx).
+		Delete(t).
+		Error; err != nil {
+		return err
+	}
+
+	return nil
 }
