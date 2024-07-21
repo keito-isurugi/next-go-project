c9dceb85076dad27959b951aa4cad421d7f6c268
keito-isurugi <ja224kg@gmail.com>
2024-07-22 03:38:54 +0900

    domain修正

--- backend/internal/domain/entity/todo.go
+++ backend/internal/domain/entity/todo.go
@@ -3,13 +3,34 @@ package entity
 import "time"
 
 type Todo struct {
-	ID        int
-	UserID    int
-	Title     string
-	DoneFlag  bool
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
+	ID             int
+	UserID         int
+	Title          string
+	AttachmentFile string
+	DoneFlag       bool
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      *time.Time
 }
 
 type ListTodos []Todo
+
+func NewTodo(
+	id int,
+	userID int,
+	title string,
+	attachmentFile string,
+	doneFlag bool,
+	createdAt time.Time,
+	updatedAt time.Time,
+) *Todo {
+	return &Todo{
+		ID:             id,
+		UserID:         userID,
+		Title:          title,
+		AttachmentFile: attachmentFile,
+		DoneFlag:       doneFlag,
+		CreatedAt:      createdAt,
+		UpdatedAt:      updatedAt,
+	}
+}
--- backend/internal/domain/todo/todo_repository.go
+++ backend/internal/domain/todo/todo_repository.go
@@ -9,4 +9,8 @@ import (
 
 type TodoRepository interface {
 	ListTodos(ctx context.Context) (entity.ListTodos, error)
+	GetTodo(ctx context.Context, id int) (entity.Todo, error)
+	RegisterTodo(ctx context.Context, todo *entity.Todo) (int, error)
+	UpdateTodo(ctx context.Context, todo *entity.Todo) error
+	DeleteTodo(ctx context.Context, id int) error
 }
